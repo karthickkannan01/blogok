079073e756ad8136d729d42ed6502e74d6f0f809
karthickkannan01 <sundarkarthi2001@gmail.com>
2020-05-29 06:31:21 -0700

    first commit

--- blogok/models.py
+++ blogok/models.py
@@ -17,6 +17,18 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})
 
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     images = models.ImageField(default='default.jpg', null=False, blank=False, upload_to='profile_pics')
